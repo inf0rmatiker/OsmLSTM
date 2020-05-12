797204d13f8d5916201943c42acb54eeb5302edf
Caleb <ccarlson355@gmail.com>
2020-05-12 10:18:37 -0600

    Preprocess zoom level

--- load_data.py
+++ load_data.py
@@ -2,90 +2,103 @@ import torch
 import numpy as np
 import os
 
+def find_max_count():
+    max_count = 0
+    files = os.listdir('./zoom_1_subset')  # Sort by date
+    for file in files:
+        filename = './zoom_1_subset/' + file 
+        counts = get_counts(filename)
+        for count in counts:
+            if count > max_count:
+                max_count = count
+
+    return max_count
+
 def get_counts(filename):
-	line_patterns = [
-		'1,0,0,0,',
-		'1,0,1,2,',
-		'1,1,0,1,',
-		'1,1,1,3,'
-	]
-
-	# Read first four lines of file, producing [c_i0, c_i2, c_i1, c_i3]
-	with open(filename, 'r') as f:
-		counts = []
-		for tile_entry in range(4):
-			line = f.readline().strip()
-
-			# Handle bad data
-			if line[:8] == line_patterns[tile_entry]:
-				vals = [int(x) for x in line.split(',')[4:]] # Only capture counts
-				counts.append(vals[0])
-			else:
-				counts.append(0)
-
-		return counts
+    line_patterns = [
+      '1,0,0,0,',
+      '1,0,1,2,',
+      '1,1,0,1,',
+      '1,1,1,3,'
+    ]
+  
+    # Read first four lines of file, producing [c_i0, c_i2, c_i1, c_i3]
+    with open(filename, 'r') as f:
+        counts = []
+        for tile_entry in range(4):
+          line = f.readline().strip()
+  
+          # Handle bad data
+          if line[:8] == line_patterns[tile_entry]:
+            vals = [int(x) for x in line.split(',')[4:]] # Capture counts
+            counts.append(vals[0])
+          else:
+            counts.append(0)
+  
+        return counts
 
 
 '''
 Returns training, validation, and test sets, each of which contain:
 [
-	[
-		tensor([ <-- window_size (i) * count vectors
-			[c_00, c_02, c_01, c_03],
-			[c_10, c_12, c_11, c_13],
-			...
-			[c_i0, c_i2, c_i1, c_i3]
-		]), 
-		tensor([c_(i+1)0, c_(i+1)2, c_(i+1)1, c_(i+1)3]) <-- label for i+1 count vector
-	],
-	...
-	[]
+  [
+    tensor([ <-- window_size (i) * count vectors
+      [c_00, c_02, c_01, c_03],
+      [c_10, c_12, c_11, c_13],
+      ...
+      [c_i0, c_i2, c_i1, c_i3]
+    ]), 
+    tensor([c_(i+1)0, c_(i+1)2, c_(i+1)1, c_(i+1)3]) <-- label for i+1 count vector
+  ],
+  ...
+  []
 ]
 '''
-def get_data_sets(window_size):
-	train_features = []
-	train_targets = []
-	validation_features = []
-	validation_targets = []
-	test_features = []
-	test_targets = []
-
-	window_start = 0
-	window_end = window_size
-
-
-	files = os.listdir('./zoom_1_subset')  # Sort by date
-	files.sort()
-	while window_end < (len(files) - 1):
-
-		input_vector = []
-
-		# Read input_size files
-		for i in range(window_start, window_end):
-			file = './zoom_1_subset/' + files[i]
-			input_vector.append(get_counts(file))
-
-		# Create tensor from input_size vectors?
-		numpy_input = np.array(input_vector)
-		tensor_input = torch.from_numpy(numpy_input).float()
-
-		file = './zoom_1_subset/' + files[window_end]
-		numpy_output = np.array(get_counts(file))
-		tensor_output = torch.from_numpy(numpy_output).float()
-
-		year = int(file[22:26])
-
-		if year < 2016:
-			train_features.append(tensor_input)
-			train_targets.append(tensor_output)
-		elif year < 2018:
-			validation_features.append(tensor_input)
-			validation_targets.append(tensor_output)
-		else:
-			test_features.append(tensor_input)
-			test_targets.append(tensor_output)
-
-		window_start += 1
-		window_end += 1
-			
-	return torch.stack(train_features), torch.stack(train_targets), torch.stack(validation_features), torch.stack(validation_targets), torch.stack(test_features), torch.stack(test_targets)
+def get_data_sets(window_size, max_count):
+    train_features = []
+    train_targets = []
+    validation_features = []
+    validation_targets = []
+    test_features = []
+    test_targets = []
+  
+    window_start = 0
+    window_end = window_size
+ 
+    files = os.listdir('./zoom_1_subset')  # Sort by date
+    files.sort()
+    while window_end < (len(files) - 1):
+  
+        input_vector = []
+    
+        # Read input_size files
+        for i in range(window_start, window_end):
+            file = './zoom_1_subset/' + files[i]
+            counts = [(float(x) / max_count) for x in get_counts(file)] # Normalize counts between 0 and 1
+            input_vector.append(counts)
+    
+        # Create tensor from input_size vectors?
+        numpy_input = np.array(input_vector)
+        tensor_input = torch.from_numpy(numpy_input).float()
+    
+        file = './zoom_1_subset/' + files[window_end]
+        counts = [(float(x) / max_count) for x in get_counts(file)] # Normalize counts between 0 and 1
+        numpy_output = np.array(counts)
+        tensor_output = torch.from_numpy(numpy_output).float()
+    
+        year = int(file[22:26])
+    
+        if year < 2016:
+            train_features.append(tensor_input)
+            train_targets.append(tensor_output)
+        elif year < 2018:
+            validation_features.append(tensor_input)
+            validation_targets.append(tensor_output)
+        else:
+            test_features.append(tensor_input)
+            test_targets.append(tensor_output)
+    
+        window_start += 1
+        window_end += 1
+        
+    return torch.stack(train_features), torch.stack(train_targets), torch.stack(validation_features), torch.stack(validation_targets), torch.stack(test_features), torch.stack(test_targets)
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import load_data
 import numpy as np
 import os
-
+import matplotlib
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,14 +16,14 @@ zoom,x,y,quadhash,count
 There are 4 entries per day, one per quadhash tile at zoom level 1.
 
 Tiles are laid out as follows for zoom level 1:
-	00, 01, 10, 11 for xy coordinates
+    00, 01, 10, 11 for xy coordinates
 y
 ^_______________
-|       |		|
-| (2)01	| (3)11	|
+|       |       |
+| (2)01 | (3)11 |
 |_______|_______|
-|		|		|
-| (0)00	| (1)10	|
+|       |       |
+| (0)00 | (1)10 |
 |_______|_______| > x
 
 
@@ -32,96 +33,114 @@ used to create LSTM class.
 '''
 class LSTM(nn.Module):
 
-	''' input_size
-			Size of input tensor (number of features). For zoom level 1, this is 4.
+    ''' input_size
+            Size of input tensor (number of features). For zoom level 1, this is 4.
 
-		window_size
-			How many days are input.
-		
-		hidden_size
-			How many neurons are in each hidden layers.
+        window_size
+            How many days are input.
+        
+        hidden_size
+            How many neurons are in each hidden layers.
 
-		output_size
-			How many neurons in the output (how many predictions). For zoom level 1, this is 4.
+        output_size
+            How many neurons in the output (how many predictions). For zoom level 1, this is 4.
 
-	'''
-	def __init__(self, input_size=4, window_size=1, hidden_size=4, output_size=4):
-		super(LSTM, self).__init__()
-		self.input_size  = input_size
-		self.hidden_size = hidden_size
-		self.output_size = output_size
+    '''
+    def __init__(self, input_size=4, window_size=1, hidden_size=4, output_size=4):
+        super(LSTM, self).__init__()
+        self.input_size  = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
 
-		# LSTM algorithm accepts 3 inputs: previous hidden state, previous cell state, and current input.
-		self.lstm   = nn.LSTM(input_size * window_size, hidden_size) # LSTM layer: input -> hidden_layer
-		self.linear = nn.Linear(hidden_size, output_size) # Linear layer: hidden_layer -> output
+        # LSTM algorithm accepts 3 inputs: previous hidden state, previous cell state, and current input.
+        self.lstm   = nn.LSTM(input_size * window_size, hidden_size) # LSTM layer: input -> hidden_layer
+        self.linear = nn.Linear(hidden_size, output_size) # Linear layer: hidden_layer -> output
 
-		# Contains the previous hidden and cell state.
-		self.hidden_cell = (torch.zeros(1, 1, self.hidden_size),
-							torch.zeros(1, 1, self.hidden_size))
+        # Contains the previous hidden and cell state.
+        self.hidden_cell = (torch.zeros(1, 1, self.hidden_size),
+                            torch.zeros(1, 1, self.hidden_size))
 
-	def forward(self, input_seq):
-		# Pass input_seq into LSTM layer, which outputs hidden and cell states at current time step.
-		lstm_output, self.hidden_cell = self.lstm(input_seq.view(len(input_seq),1,-1), self.hidden_cell)
-		predictions = self.linear(lstm_output.view(len(input_seq),-1))
-		return predictions
+    def forward(self, input_seq):
+        # Pass input_seq into LSTM layer, which outputs hidden and cell states at current time step.
+        lstm_output, self.hidden_cell = self.lstm(input_seq.view(len(input_seq),1,-1), self.hidden_cell)
+        predictions = self.linear(lstm_output.view(len(input_seq),-1))
+        return predictions
 
 
-
-		
+def plot_labels(number, labels):
+    
+        
 def main():
-	# Parameters for LSTM
-	WINDOW_SIZE = 1
-	INPUT_SIZE  = 4
-	HIDDEN_SIZE = 128
-	OUTPUT_SIZE = 4
-	EPOCHS = 1
+    # Parameters for LSTM
+    WINDOW_SIZE = 10 
+    INPUT_SIZE  = 4
+    HIDDEN_SIZE = 4 
+    OUTPUT_SIZE = 4
+    EPOCHS = 10 
 
-	train_f, train_t, valid_f, valid_t, test_f, test_t = load_data.get_data_sets(WINDOW_SIZE)
 
-	train_set 		= torch.utils.data.TensorDataset(train_f, train_t)
-	valid_set 		= torch.utils.data.TensorDataset(valid_f, valid_t)
-	test_set  		= torch.utils.data.TensorDataset(test_f, test_t)
+    max_count = load_data.find_max_count()
+    train_f, train_t, valid_f, valid_t, test_f, test_t = load_data.get_data_sets(WINDOW_SIZE, max_count)
 
-	train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False)
-	valid_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False)
-	test_loader  = torch.utils.data.DataLoader(test_set,  batch_size=1, shuffle=False)
+    train_set         = torch.utils.data.TensorDataset(train_f, train_t)
+    valid_set         = torch.utils.data.TensorDataset(valid_f, valid_t)
+    test_set          = torch.utils.data.TensorDataset(test_f, test_t)
 
-	
-	model = LSTM(INPUT_SIZE, WINDOW_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
-	loss_func = nn.MSELoss()
-	optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False)
+    valid_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False)
+    test_loader  = torch.utils.data.DataLoader(test_set,  batch_size=1, shuffle=False)
 
-	iteration = 0
-	iteration_break = 1
+    
+    model = LSTM(INPUT_SIZE, WINDOW_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    loss_func = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-	for epoch in range(EPOCHS):
-		for seq, labels in train_loader:
-		
-			optimizer.zero_grad()
-			model.hidden_cell = (torch.zeros(1, 1, model.hidden_size), 
-								 torch.zeros(1, 1, model.hidden_size))
-			
-			y_prediction = model(seq)
+    iteration = 0
+    iteration_break = -1
+    interval_print = -1
 
-			single_loss = loss_func(y_prediction, labels) # calculate loss for single prediction
-			single_loss.backward() # propagate loss backward
-			optimizer.step()
+    for epoch in range(EPOCHS):
+        for seq, labels in train_loader:
+        
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size), 
+                                 torch.zeros(1, 1, model.hidden_size))
+            
+            y_prediction = model(seq)
 
-			print('Iteration %d' % iteration)
-			print('Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
-			print('Loss (MSE): %s\n' % single_loss.item())
-			
-			if (iteration == iteration_break):
-				break
-			
-			iteration += 1
+            single_loss = loss_func(y_prediction, labels) # calculate loss for single prediction
+            single_loss.backward() # propagate loss backward
+            optimizer.step()
 
-		if (epoch % 1 == 0):
-			print('Epoch: %d: loss: %10.8f' % (epoch, single_loss.item()))
+            #if (iteration % interval_print == 0):
+            #    print('Iteration %d' % iteration)
+            #    print('Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
+            #    print('Loss (MSE): %s\n' % single_loss.item())
+            
+            if (iteration == iteration_break):
+                break
+            
+            iteration += 1
 
-		
-		
+        if (epoch % 1 == 0):
+            print('Epoch: %d: loss: %10.8f' % (epoch, single_loss.item()))
+
+    model.eval() 
+    for seq, labels in valid_loader:
+        with torch.no_grad():
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size), 
+                                 torch.zeros(1, 1, model.hidden_size))
+
+            y_prediction = model(seq)
+            single_loss = loss_func(y_prediction, labels)
+
+            print('TEST Iteration %d' % iteration)
+            print('TEST Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
+            print('TEST Loss (MSE): %s\n' % single_loss.item())
+            print(int(y_prediction[0][0]*max_count))
+            print(int(labels[0][0]*max_count))
+            break
 
 
 if __name__ == '__main__':
-	main()
+    main()
