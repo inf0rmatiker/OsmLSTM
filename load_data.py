b62704aa714100bcad1c8219dded73e680309801
Caleb <ccarlson355@gmail.com>
2020-05-25 11:46:38 -0600

    Normalize data

--- load_data.py
+++ load_data.py
@@ -14,7 +14,7 @@ bottom-right x,y of [324,438]:
     |                   |
     |                   |
  |  |                   |
- v  |___________________| 
+ v  |___________________|
                         324,438
 '''
 
@@ -23,8 +23,8 @@ bottom-right x,y of [324,438]:
 TOP_LEFT_CORNER_X = 159
 TOP_LEFT_CORNER_Y = 351
 BOTTOM_RIGHT_CORNER_X = 324
-BOTTOM_RIGHT_CORNER_Y = 438 
-
+BOTTOM_RIGHT_CORNER_Y = 438
+ZOOM_LEVEL = 10
 X_WIDTH  = BOTTOM_RIGHT_CORNER_X - TOP_LEFT_CORNER_X # 165
 Y_HEIGHT = BOTTOM_RIGHT_CORNER_Y - TOP_LEFT_CORNER_Y # 87
 
@@ -34,15 +34,26 @@ Y_HEIGHT = BOTTOM_RIGHT_CORNER_Y - TOP_LEFT_CORNER_Y # 87
     between 0 and 1.
 '''
 def find_max_count(directory):
+    print("Finding max count for normalization...")
+
+    index = 0
     max_count = 0
     files = os.listdir(directory)  # Sort by date
     for filename in files:
+        if ((index+1)%10) == 0:
+            print("\e[\rProcessing file %d/%d" % (index+1, 2096))
+        elif index == 0:
+            print("Processing file 0/2096")
+
+
         filename = directory + filename
         with open(filename, 'r') as f:
             for line in f:
                 vals = [int(x) for x in line.split(',')]
                 if vals[4] > max_count:
                     max_count = vals[4]
+        index += 1
+
 
     print('Max count value of %d found.' % max_count)
     return max_count
@@ -57,18 +68,19 @@ def get_counts(filename, max_count):
     # Initialize matrix for all possible tiles within area
     #  specified by Y_HEIGHT and X_WIDTH (rows, cols)
     counts = np.zeros((Y_HEIGHT, X_WIDTH), dtype=float)
-    
+
     with open(filename, 'r') as f:
-        for tile_entry in range(4):
+        for line in f:
             line = f.readline().strip()
-            
-            if len(line) > 0: 
+
+            if len(line) > 0:
                 vals = [int(x) for x in line.split(',')] # Capture counts
-                x = vals[1] - TOP_LEFT_CORNER_X
-                y = vals[2] - TOP_LEFT_CORNER_Y
-                count = float(vals[4]) / max_count
-      
-                counts[y][x] = count
+                if vals[0] == ZOOM_LEVEL:
+                    x = vals[1] - TOP_LEFT_CORNER_X - 1
+                    y = vals[2] - TOP_LEFT_CORNER_Y - 1
+                    count = float(vals[4]) / max_count # Normalize count with max_count
+
+                    counts[y][x] = count
 
         return counts
 
@@ -82,39 +94,53 @@ def get_counts(filename, max_count):
     Returns torch stacked numpy arrays for train/validation/test sets.
 '''
 def get_data_sets(directory, window_size, max_count):
+    cuda_available = torch.cuda.is_available()
+    type = torch.FloatTensor
+    if (cuda_available):
+        type = torch.cuda.FloatTensor
+
+    if cuda_available:
+        print("CUDA is available")
+    else:
+        print("CUDA is not available")
+    #print("CUDA %s available." % 'is' if cuda_available else 'is not')
+
     train_features = []
     train_targets = []
     validation_features = []
     validation_targets = []
     test_features = []
     test_targets = []
-  
+
     window_start = 0
     window_end = window_size
- 
+
     files = os.listdir(directory)
     files.sort()  # Sort by date
     while window_end < (len(files) - 1):
- 
+
+        if (window_start+1) % 10 == 0:
+            print("Processing file %d/%d" % (window_start+1, 2096))
+
         # List of numpy matrices      
         input_vector = []
-    
+
         # Read input_size files
         for i in range(window_start, window_end):
             file = directory + files[i]
-            counts = get_counts(file, max_count) 
+            counts = get_counts(file, max_count)
             input_vector.append(counts)
-    
+
         numpy_input = np.array(input_vector)
-        tensor_input = torch.from_numpy(numpy_input)
-    
+        tensor_input = torch.from_numpy(numpy_input).type(type)
+
         file = directory + files[window_end]
-        counts = get_counts(file, max_count) 
+        counts = get_counts(file, max_count)
         numpy_output = np.array(counts)
-        tensor_output = torch.from_numpy(numpy_output)
-    
+        tensor_output = torch.from_numpy(numpy_output).type(type)
+
         year = int(file[23:27])
-    
+
         if year < 2016:
             train_features.append(tensor_input)
             train_targets.append(tensor_output)
@@ -124,8 +150,8 @@ def get_data_sets(directory, window_size, max_count):
         else:
             test_features.append(tensor_input)
             test_targets.append(tensor_output)
-    
+
         window_start += 1
         window_end += 1
-        
+
     return torch.stack(train_features), torch.stack(train_targets), torch.stack(validation_features), torch.stack(validation_targets), torch.stack(test_features), torch.stack(test_targets)
--- train.py
+++ train.py
@@ -7,6 +7,13 @@ import torch.nn.functional as F
 from   torch.autograd import Variable
 
 
+
+def print_tensor(tensor, start, end):
+        print('[', end="", flush=True)
+        for i in range(start, end):
+            print('%.8f ' % tensor[i].item(), end="", flush=True)
+        print(']')
+
 '''
 Note: tutorial from https://stackabuse.com/time-series-prediction-using-lstm-with-pytorch-in-python/
 used to create LSTM class.
@@ -19,7 +26,7 @@ class LSTM(nn.Module):
 
         window_size
             How many days are input.
-        
+
         hidden_size
             How many neurons are in each hidden layers.
 
@@ -27,14 +34,14 @@ class LSTM(nn.Module):
             How many neurons in the output (how many predictions). For zoom level 1, this is 4.
 
     '''
-    def __init__(self, input_size=4, window_size=1, hidden_size=4, output_size=4):
+    def __init__(self, input_size, window_size=1, hidden_size=4, output_size=4):
         super(LSTM, self).__init__()
         self.input_size  = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
         # LSTM algorithm accepts 3 inputs: previous hidden state, previous cell state, and current input.
-        self.lstm   = nn.LSTM(input_size * window_size, hidden_size) # LSTM layer: input -> hidden_layer
+        self.lstm   = nn.LSTM(input_size, hidden_size) # LSTM layer: input -> hidden_layer
         self.linear = nn.Linear(hidden_size, output_size) # Linear layer: hidden_layer -> output
 
         # Contains the previous hidden and cell state.
@@ -43,27 +50,28 @@ class LSTM(nn.Module):
 
     def forward(self, input_seq):
         # Pass input_seq into LSTM layer, which outputs hidden and cell states at current time step.
-        lstm_output, self.hidden_cell = self.lstm(input_seq.view(len(input_seq),1,-1), self.hidden_cell)
-        predictions = self.linear(lstm_output.view(len(input_seq),-1))
-        return predictions
+        lstm_output, self.hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_output.view(len(input_seq), 1, -1))
+        return predictions[-1]
 
 
-    
-        
 def main():
 
+    X_WIDTH  = 165
+    Y_HEIGHT = 87
+
     # Variables for LSTM
-    WINDOW_SIZE = 10 
-    INPUT_SIZE  = 4
-    HIDDEN_SIZE = 64 
-    OUTPUT_SIZE = 4
-    EPOCHS = 10 
+    WINDOW_SIZE = 10
+    INPUT_SIZE  = X_WIDTH * Y_HEIGHT
+    HIDDEN_SIZE = 128
+    OUTPUT_SIZE = X_WIDTH * Y_HEIGHT
+    EPOCHS = 25
 
     # Variables for data loader
     DIRECTORY = './zoom_10_subset/'
-    
+
     max_count = load_data.find_max_count(DIRECTORY)
-    
+
     train_f, train_t, valid_f, valid_t, test_f, test_t = load_data.get_data_sets(DIRECTORY, WINDOW_SIZE, max_count)
 
     train_set    = torch.utils.data.TensorDataset(train_f, train_t)
@@ -74,57 +82,66 @@ def main():
     valid_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False)
     test_loader  = torch.utils.data.DataLoader(test_set,  batch_size=1, shuffle=False)
 
-
-
-    for data in train_set:
-        print(data[0].size())
-        break
-
-
     '''
-        
     model = LSTM(INPUT_SIZE, WINDOW_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
     loss_func = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     iteration = 0
     iteration_break = -1
-    interval_print = -1
+    interval_print  = -1
+
 
     for epoch in range(EPOCHS):
         for seq, labels in train_loader:
-        
+            seq = seq.view(-1, Y_HEIGHT*X_WIDTH)
+            labels = labels.view(-1, Y_HEIGHT*X_WIDTH)
+
             optimizer.zero_grad()
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size), 
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size),
                                  torch.zeros(1, 1, model.hidden_size))
-            
+
             y_prediction = model(seq)
 
             single_loss = loss_func(y_prediction, labels) # calculate loss for single prediction
             single_loss.backward() # propagate loss backward
             optimizer.step()
 
-            #if (iteration % interval_print == 0):
-            #    print('Iteration %d' % iteration)
-            #    print('Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
-            #    print('Loss (MSE): %s\n' % single_loss.item())
-            
+            seq = seq * max_count
+            labels = labels * max_count
+            y_prediction = y_prediction * max_count
+
+            if (iteration % interval_print == 0):
+                print('Iteration %d' % iteration)
+                print('Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
+                print('Loss (MSE): %s\n' % single_loss.item())
+                #print_tensor(labels[0], 4000, 4050)
+                #print_tensor(y_prediction, 30)
+
             if (iteration == iteration_break):
                 break
-            
+
             iteration += 1
 
-        if (epoch % 1 == 0):
+        if (epoch % 5 == 0):
             print('Epoch: %d: loss: %10.8f' % (epoch, single_loss.item()))
 
-    model.eval() 
+    model.eval()
     for seq, labels in valid_loader:
         with torch.no_grad():
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size), 
+            seq = seq.view(-1, Y_HEIGHT*X_WIDTH)
+            labels = labels.view(-1, Y_HEIGHT*X_WIDTH)
+
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size),
                                  torch.zeros(1, 1, model.hidden_size))
 
             y_prediction = model(seq)
             single_loss = loss_func(y_prediction, labels)
+
+            seq = seq * max_count
+            labels = labels * max_count
+            y_prediction = y_prediction * max_count
+
 
             print('TEST Iteration %d' % iteration)
             print('TEST Sequence:   %s\nLabels:     %s\nPrediction: %s' % (str(seq), str(labels), str(y_prediction)) )
